556c8cbd1bfd47e852924cf65d283b8e556b1ca5
James E. Jones <james.jones@puppet.com>
2016-10-14 16:20:52 -0500

    update web

--- site/profile/manifests/rgbank/web.pp
+++ site/profile/manifests/rgbank/web.pp
@@ -6,13 +6,11 @@ class profile::rgbank::web (
   $db_password,
 ) {
   include profile::common
-  include profile::apache::remove
   include profile::nginx
   include profile::mysql::client
   include profile::git
 
   Class['profile::common'] -> Class['profile::nginx']
-  Class['profile::apache::remove'] -> Class['profile::nginx']
 
   rgbank::web { "web-app":
     db_name     => $db_name,
