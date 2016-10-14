6297b8409da2de308a29050f3586876a8ec52d46
James E. Jones <james.jones@puppet.com>
2016-10-14 16:52:39 -0500

    remove case statement since using more up to date module version

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -4,12 +4,5 @@ class profile::apache(
   class { 'apache':
     default_vhost => $default_vhost,
   }
-  if $::osfamily == 'Debian' {
-    class { 'apache::mod::php':
-      php_version => '7',
-    }
-  }
-  else {
-    class { 'apache::mod::php': }
-  }
+  class { 'apache::mod::php': }
 }
