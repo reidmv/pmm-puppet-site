d6e8f86b19088a61741c88455eb86068533cca2a
Reid Vandewiele <reid@puppetlabs.com>
2016-10-14 15:56:14 -0700

    Clean up puppetdbquery

--- site/role/manifests/rgbank/web.pp
+++ site/role/manifests/rgbank/web.pp
@@ -3,17 +3,21 @@ class role::rgbank::web {
   include profile::nginx
   include profile::mysql::client
 
+  $db_address = puppetdb_query("
+    facts[value] {
+      name = \"ipaddress\" and certname in inventory[certname] {
+        trusted.extensions.pp_application = \"${trusted['extensions']['pp_application']}\" and
+        trusted.extensions.pp_cluster = \"${trusted['extensions']['pp_cluster']}\" and
+        trusted.extensions.pp_role = \"DatabaseNode\"
+      }
+    }
+  ")[0]['value']
+
   class { 'profile::rgbank::web':
     db_name     => 'rgbank',
     db_user     => 'rgbank',
     db_password => 'rgbankPassw0rd',
-    db_host     => puppetdb_query("facts[value] {
-                                    name = \"ipaddress\" and certname in inventory[certname] {
-                                      trusted.extensions.pp_application = \"${trusted['extensions']['pp_application']}\" and
-                                      trusted.extensions.pp_cluster = \"${trusted['extensions']['pp_cluster']}\" and
-                                      trusted.extensions.pp_role = \"DatabaseNode\"
-                                    }
-                                  }")[0]['value'],
+    db_host     => $db_address,
   }
 
 }
