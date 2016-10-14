c3c87157e2956382557daf95114a35522f58112d
Chris Matteson <chris.matteson@gmail.com>
2016-10-13 17:53:14 -0700

    added ensure parameter

--- site/profile/manifests/rgbank/dev.pp
+++ site/profile/manifests/rgbank/dev.pp
@@ -1,9 +1,11 @@
 class profile::rgbank::dev (
+  $ensure = undef,
   $source = undef,
   $revision = undef,
 ) {
 
   Vcsrepo <| tag == 'rgbank::web' |> {
+    ensure   => $ensure,
     source   => $source,
     revision => $revision,
     force    => true,
