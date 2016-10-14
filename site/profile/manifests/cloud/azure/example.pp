71c4600c7872458f5bd3c2a67141a1e501455faf
James E. Jones <james.jones@puppet.com>
2016-10-14 18:52:49 -0500

    update example

--- site/profile/manifests/cloud/azure/example.pp
+++ site/profile/manifests/cloud/azure/example.pp
@@ -4,21 +4,21 @@ class profile::cloud::azure::example (
 )
 {
 
-  profile::cloud::azure::linux { 'monolithic':
+  profile::cloud::azure::linux { 'monolithic1':
     port       => '8060',
     agent_role => 'rgbank_monolithic',
     deployment => $deployment,
     cluster    => "${cluster}-monolithic",
   }
 
-  profile::cloud::azure::linux { 'webserver':
+  profile::cloud::azure::linux { 'webserver1':
     port       => '8060',
     agent_role => 'webserver',
     deployment => $deployment,
     cluster    => $cluster,
   }
   
-  profile::cloud::azure::linux { 'dbserver':
+  profile::cloud::azure::linux { 'dbserver1':
     port       => '3306',
     agent_role => 'DatabaseNode',
     deployment => $deployment,
