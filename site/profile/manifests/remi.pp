c14fc7c993a2f247e9ff8db54dd904fee7f01ab8
Carl Caum <carl@carlcaum.com>
2016-10-14 20:53:41 -0700

    Use package to set up remi repository

--- site/profile/manifests/remi.pp
+++ site/profile/manifests/remi.pp
@@ -1,9 +1,7 @@
 class profile::remi {
-  yumrepo { 'remi':
-    enabled    => 1,
-    mirrorlist => 'http://rpms.remirepo.net/enterprise/7/remi/mirror',
-    descr      => 'Remi\'s RPM repository for Enterprise Linux 7 - $basearch',
-    gpgcheck   => 1,
-    gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-remi',
+  package { 'remi-repository':
+    source   => 'http://rpms.famillecollet.com/enterprise/remi-release-7.rpm',
+    provider => 'rpm',
+    ensure   => installed,
   }
 }
