9eddf6cf200224a3e6e60c336f58fce724e9f3c2
Carl Caum <carl@carlcaum.com>
2016-10-14 20:47:05 -0700

    use proper description parameter for yumrepo

--- site/profile/manifests/remi.pp
+++ site/profile/manifests/remi.pp
@@ -2,7 +2,7 @@ class profile::remi {
   yumrepo { 'remi':
     enabled    => 1,
     mirrorlist => 'http://rpms.remirepo.net/enterprise/7/remi/mirror',
-    name       => 'Remi\'s RPM repository for Enterprise Linux 7 - $basearch',
+    descr      => 'Remi\'s RPM repository for Enterprise Linux 7 - $basearch',
     gpgcheck   => 1,
     gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-remi',
   }
