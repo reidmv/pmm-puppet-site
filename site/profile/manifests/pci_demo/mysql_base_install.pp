0ce3faf7bf92401e6acc88b6c79cf1516577d15c
ssorb <shane.sorbello@puppetlabs.com>
2016-10-17 16:47:39 -0400

    made sensitive hiera call

--- site/profile/manifests/pci_demo/mysql_base_install.pp
+++ site/profile/manifests/pci_demo/mysql_base_install.pp
@@ -4,11 +4,11 @@ class profile::pci_demo::mysql_base_install (
   ##PCI Example: Protect Data##
   #############################
 
-  Sensitive[String] $mysql_root_password,
+  Sensitive[String] $m_r_p = Sensitive(hiera($mysql_root_password)),
 ){
   class { '::mysql::server':
     create_root_user        => true,
-    root_password           => Sensitive(hiera($mysql_root_password)),
+    root_password           => $m_r_p,
     remove_default_accounts => true,
   }
 
