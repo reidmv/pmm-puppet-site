ff2112689b26830070885433cd564fb710b266a3
ssorb <shane.sorbello@puppetlabs.com>
2016-10-17 12:16:11 -0400

    added comment

--- site/profile/manifests/pci_demo/mysql_base_install.pp
+++ site/profile/manifests/pci_demo/mysql_base_install.pp
@@ -1,5 +1,9 @@
 class profile::pci_demo::mysql_base_install (
-  #PCI Example: Protect Data
+
+  #############################
+  ##PCI Example: Protect Data##
+  #############################
+
   Sensitive[String] $mysql_root_password,
 ){
   class { '::mysql::server':
